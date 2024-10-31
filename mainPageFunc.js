29df420fe48d77af06f40d2bf2fdb3f1b26ead09
Broney715 <Broney715@gmail.com>
2024-10-31 17:12:08 -0400

    Fixed double sliders and added new prefCSS which strictly deals with css of preferences section

--- mainPageFunc.js
+++ mainPageFunc.js
@@ -7,18 +7,53 @@ $(document).ready(function(){
     });
 
     // Functions for changing each input for the Preference Sliders
-    const $tempInput = $('#tempPref');
-    const $tempValue = $('#tempValue');
+
+    let $minTempPref = $('#minTempPref');
+    let $maxTempPref = $('#maxTempPref');
+    let $minTempValue = $('#minTempValue');
+    let $maxTempValue = $('#maxTempValue');
+    let minGap = 1;
+    const sliderRange = $('#slider-range');
+    const maxTemp = $maxTempPref.attr('max');
+
+    function minSlide(){
+        if(parseInt($maxTempPref.val())- parseInt($minTempPref.val()) <= minGap){
+            $minTempPref.val(parseInt($maxTempPref.val()));
+        }
+        $minTempValue.text($minTempPref.val());
+        fillColor();
+    }
+
+
+    function maxSlide(){
+        if(parseInt($maxTempPref.val())- parseInt($minTempPref.val()) <= minGap){
+            $maxTempPref.val(parseInt($minTempPref.val()));
+        }
+        $maxTempValue.text($maxTempPref.val());
+        fillColor();
+    }
+
+    function fillColor(){
+        const percentMin = (($minTempPref.val()-20) / (maxTemp-20)) *100;
+        const percentMax = (($maxTempPref.val()-20) / (maxTemp-20)) *100;
+        sliderRange.css({
+            left: `${percentMin}%`,
+            width:`${percentMax-percentMin}%`
+        })
+        
+    }
+    minSlide();
+    maxSlide();
+    fillColor();
+
+    $minTempPref.on('input',minSlide);
+    $maxTempPref.on('input',maxSlide);
 
     const $precipInput = $('#precipPref');
     const $precipValue = $('#precipValue');
 
     const $humidInput = $('#humidPref');
     const $humidValue = $('#humidValue');
-
-    $tempInput.on('input', function(){
-        $tempValue.text($(this).val());
-    });
 
     $precipInput.on('input', function(){
         $precipValue.text($(this).val());
